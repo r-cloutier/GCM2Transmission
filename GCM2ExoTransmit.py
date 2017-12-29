d8375ad82aa8b58f2a5c521edb50b24eb43fcdc1
Ryan Cloutier <cloutier@astro.utoronto.ca>
2017-12-28 19:02:29 -0500

    working on interpolation functions to get GCM parameters along the ray

--- GCM2ExoTransmit.py
+++ GCM2ExoTransmit.py
@@ -27,7 +27,7 @@ def main():
 
             h_ray, lon_ray, lat_ray = _cart2geo3D(x_ray, y_ray, z_ray)
 
-            _write_EOS_file(h, lon, lat, Ps, P, T, X_H2O,
+            _write_EOS_file(time, h, lon, lat, P, T, X_H2O,
                             h_ray, lon_ray, lat_ray)
             
             
@@ -103,22 +103,61 @@ def _cart2sphere3D(x, y, z):
     return r, phi, theta
 
 
-def _write_EOS_file(h, lon, lat, Ps, P, T, X_H2O,
-                    h_ray, lon_ray, lat_ray):
+def _write_EOS_file(time, h, lon, lat, P, T, X_H2O,
+                    h_ray, lon_ray, lat_ray, time_ray=0):
     '''
     Write the ExoTransmit EOS file based on the GCM output and the ray path.
     '''
     # get GCM output along the ray
-    _get_GCM_variables_along_ray(h, lon, lat, Ps, P, T, X_H2O,
-                                 h_ray, lon_ray, lat_ray)
+    P_ray, T_ray, X_ray = _get_GCM_variables_along_ray(time, h, lon, lat, P, T,
+                                                       X_H2O, h_ray, lon_ray,
+                                                       lat_ray, time_ray)
 
     # write EOS file
     
+    
 
-def _get_GCM_variables_along_ray(h, lon, lat, Ps, P, T, X_H2O,
-                                 h_ray, lon_ray, lat_ray):
+def _get_GCM_variables_along_ray(time, h, lon, lat, P, T, X_H2O,
+                                 h_ray, lon_ray, lat_ray, time_ray=0):
     '''
     Interpolate along the ray path to get GCM varaiables of interest. 
     '''
+    # form grid of spatial and temporal points
+    Ntime, NP, Nlat, Nlon = T.shape
+    Npnts = np.product(T.shape)
+    points = np.zeros((Npnts, 4))
+    points[:,0] = np.ascontiguousarray(list(time) * NP * Nlat * Nlon)    
+    points[:,1] = h.reshape(Npnts)
+    points[:,2] = np.ascontiguousarray(list(lat) * Ntime * NP * Nlon)
+    points[:,3] = np.ascontiguousarray(list(lon) * Ntime * NP * Nlat)
+    
+    # interpolate to get pressures
+    P4 = np.repeat(np.repeat(np.repeat(P[np.newaxis,:], Ntime,
+                                       axis=0)[:,:,np.newaxis], Nlat,
+                             axis=2)[:,:,:,np.newaxis], Nlon, axis=3)
+    assert P4.shape == T.shape
+    Pint = lint(points, P4.reshape(Npnts))
+    P_ray = Pint(time_ray, h_ray, lon_ray, lat_ray)
+    
+    # interpolate to get temperatures
+    Tint = lint(points, T.reshape(Npnts))
+    T_ray = Tint(time_ray, h_ray, lon_ray, lat_ray)
+
+    # interpolate to get specific humidity
+    Xint = lint(points, X_H2O.reshape(Npnts))
+    X_ray = Xint(time_ray, h_ray, lon_ray, lat_ray)
+
+    return P_ray, T_ray, X_ray
     
     
+    
+#def _combine_P_Ps(P, Ps):
+#    '''
+#    Create full 4D array of pressures including the surface pressure by 
+#    appending it.
+#    '''
+#    Ntime, Nlat, Nlon = Ps.shape
+#    P4 = np.repeat(np.repeat(np.repeat(P[np.newaxis,:], Ntime,
+#                                       axis=0)[:,:,np.newaxis], Nlat,
+#                             axis=2)[:,:,:,np.newaxis], Nlon, axis=3)
+#    return np.insert(P4[:,::-1], 0, Ps, axis=1)[:,::-1]

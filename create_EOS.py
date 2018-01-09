b16e6172bc329555eb5d36d93ded9af1b1672bc6
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-01-09 12:08:03 -0500

    starting new approach to the problem focused on spectra first and mass-weighting maps

--- create_EOS.py
+++ create_EOS.py
@@ -1,15 +1,12 @@
 from imports import *
 
 
-def setup_Earthlike_EOS(P_cell, T_cell, H2O_cell):
+def setup_Earthlike_EOS(P_cell, T_cell, X_H2O_cell):
     '''
     Create a custom EOS file based on the approximate terrestrial atmospheric 
     abundances and variable water content for each cell that a ray is 
     transmitted through with a fixed TP-profile.
     '''
-    assert P_cell.size == T_cell.size
-    assert T_cell.size == H2O_cell.size
-
     # get template EOS file
     fname = '/Users/ryancloutier/Research/Exo_Transmit/EOS/eos_1Xsolar_gas.dat'
     f = open(fname, 'r')
@@ -17,30 +14,27 @@ def setup_Earthlike_EOS(P_cell, T_cell, H2O_cell):
     f.close()
     col_names = np.ascontiguousarray(g[0].split('\t\t')[:-1])
 
-    # create unique EOS file for each cell
-    for i in range(P_cell.size):
-        
-        for j in range(2,len(g)):
+    # create unique EOS for this cell
+    for j in range(2,len(g)):
 
-            # add abundances
-            if len(g[j]) == 495:
+        # add abundances
+        if len(g[j]) == 495:
+
+            # add fixed chemical abundances
+            Xs = np.ascontiguousarray(g[j].split('\t')[:-1])
+            Xs = _set_abundances(Xs, col_names)
                 
-                # add fixed chemical abundances
-                Xs = np.ascontiguousarray(g[j].split('\t')[:-1])
-                Xs = _set_abundances(Xs, col_names)
-                
-                # add water
-                T = float(g[j].split('\t')[0])  # K
-                P = float(g[j].split('\t')[1])  # Pa
-                Xs[16] = _get_water_abundance(T, P, T_ray[i], P_ray[i],
-                                              H2O_ray[i])
+            # add water
+            T = float(g[j].split('\t')[0])  # K
+            P = float(g[j].split('\t')[1])  # Pa
+            Xs[16] = _set_water_abundance(X_H2O_cell, Xs[2:])
 
-                # check abundances
-                if Xs.sum() > 1:
-                    raise ValueError("Cumulative atmospheric abundances "+ \
-                                     "exceed 1.")
+            # check abundances
+            if Xs.sum() > 1:
+                raise ValueError("Cumulative atmospheric abundances "+ \
+                                 "exceed 1.")
 
-            # Convert to exotransmit format
+        # Convert to exotransmit format
             
             
 
@@ -74,7 +68,11 @@ def _set_abundances(Xs, col_names):
     return Xout
 
 
-def _get_water_abundance(T, P, T_ray, P_ray, H2O_ray):
+def _set_water_abundance(Xs, X_H2O_cell, P_cell, T_cell):
     '''
+    Set water abundance in EOS file at the expense of the other chemical 
+    constituents.
+    '''
+    assert Xs.size == 36
     
-    '''
+    
